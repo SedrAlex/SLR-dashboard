3de86f5a7da6803ad3ecdf7186997eef41534a75
SedrAlex313 <124774403+SedrAlex313@users.noreply.github.com>
2023-10-08 13:35:39 -0400

    finalize the sidebar

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,28 @@
+import { CssBaseline, ThemeProvider,  } from "@mui/material";
+import {createTheme} from "@mui/material/styles"
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { themeSettings } from "theme";
+import Dashboard from "scenes/Dashboard/Dashboard";
+import Layout from "scenes/Layout/Layout";
 
 function App() {
+const mode = useSelector((state) => state.global.mode)
+const theme = useMemo(() => createTheme(themeSettings(mode)),[mode])
   return (
-    <div className="App">
-   
+    <div className="app">
+      <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Routes>
+          <Route element={<Layout />} >
+          <Route path ="/" element={<Navigate to="dashboard" replace />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          </Route>
+        </Routes>
+      </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ client/src/scenes/Layout/Layout.jsx
@@ -0,0 +1,31 @@
+import { Box, useMediaQuery } from '@mui/material'
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
+import { Outlet } from 'react-router-dom'
+import  Navbar from "components/Navbar"
+import  Sidebar from "components/Sidebar"
+
+const Layout = () => {
+  const isNonMobile  = useMediaQuery("(min-width: 600px)");
+  const [isSidebarOpen,setIsSidebarOpen] = useState(true)
+  return (
+  <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%" >
+    <Sidebar
+       isNonMobile={isNonMobile}
+       drawerWidth="250px"
+       isSidebarOpen={isSidebarOpen}
+       setIsSidebarOpen={setIsSidebarOpen}
+       />
+    <Box>
+      <Navbar 
+        isSidebarOpen={isSidebarOpen}
+        setIsSidebarOpen={setIsSidebarOpen}
+      />
+      <Outlet />
+    </Box>
+  </Box>
+  )
+}
+
+
+export default Layout
