60cdb6e9ed3a3f8ab4935e4c495fd0c1649c9a9b
SedrAlex313 <124774403+SedrAlex313@users.noreply.github.com>
2023-10-10 13:30:56 -0400

    finalize the transactions routes

--- server/index.js
+++ server/index.js
@@ -14,7 +14,8 @@ import salesRoutes from "./routes/sales.js"
 import User from "./models/User.js";
 import Product from "./models/Product.js"
 import ProductStat from "./models/ProductStat.js"
-import { dataUser,dataProduct,dataProductStat } from "./data/index.js";
+import Transaction from "./models/Transaction.js";
+import { dataUser,dataProduct,dataProductStat,dataTransaction } from "./data/index.js";
 
 /** CONFIGURATION */
 dotenv.config()
@@ -44,5 +45,6 @@ mongoose.connect(process.env.MONGO_URL,{
     //  Product.insertMany(dataProduct)
     //  ProductStat.insertMany(dataProductStat)
     // User.insertMany(dataUser)
+    // Transaction.insertMany(dataTransaction)
 })
 .catch((error) => console.log(`${error} did not connect`));
